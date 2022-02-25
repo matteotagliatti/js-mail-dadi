567022ece4607238269810c021f0a7c4fb0f1675
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-02-25 15:18:55 +0100

    dadi complete, refactoring mail

--- js/script.js
+++ js/script.js
@@ -6,15 +6,36 @@ const emails = [
   "luca@esempio.com",
 ];
 
-let mailOutput = document.getElementById("mail-output");
+const mailOutput = document.getElementById("mail-output");
 
 const promptEmail = prompt("Your email");
+let emailMessage = `Email <strong>${promptEmail}</strong> non può non accedere in quanto non presente. `;
 
 for (let i = 0; i < emails.length; i++) {
   if (emails[i] === promptEmail) {
-    let emailMessage = `Email <strong>${promptEmail}</strong> può accedere in quanto presente.`;
+    emailMessage = `Email <strong>${promptEmail}</strong> può accedere in quanto presente.`;
   }
 }
 
-emailMessage = `Email <strong>${promptEmail}</strong> non può non accedere in quanto non presente `;
 mailOutput.innerHTML = emailMessage;
+
+/* Dadi */
+
+const userNumber = document.getElementById("user-number");
+const computerNumber = document.getElementById("computer-number");
+const dadiOutput = document.getElementById("dadi-output");
+
+const userRandomNumber = Math.floor(Math.random() * 6 + 1);
+const computerRandomNumber = Math.floor(Math.random() * 6 + 1);
+
+let dadiMessage = "Parità";
+
+if (userRandomNumber > computerRandomNumber) {
+  dadiMessage = "Vince il giocatore";
+} else if (computerRandomNumber > userRandomNumber) {
+  dadiMessage = "Vince il computer";
+}
+
+userNumber.innerHTML = userRandomNumber;
+computerNumber.innerHTML = computerRandomNumber;
+dadiOutput.innerHTML = dadiMessage;
